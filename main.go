c80230e79914a401c50836d96d15c0664cfc87e0
sanfeng <sanfeng@ztouch.ai>
2022-07-05 16:55:28 +0800

    将返回结果做换行处理 已经测试通过

--- main.go
+++ main.go
@@ -47,8 +47,8 @@ func main() {
 		AppSecret: appKey,
 	}
 	agent := newAgent(client)
-	q, from, to, lang,isGolangComment := parseArgs(os.Args)
 
+	q, from, to, lang, isGolangComment := parseArgs(os.Args)
 	if lang {
 		if err := agent.Client.SetFrom(from); err != nil {
 			items.Append(&alfred.ResultElement{
@@ -90,6 +90,11 @@ func main() {
 	r, err := agent.Query(q)
 	if err != nil {
 		panic(err)
+	}
+
+	// 针对返回的数据做处理 t.Translation
+	for i, translation := range *r.Translation {
+		(*r.Translation)[i] = newLine(translation, newLineLimit)
 	}
 
 	mod := map[string]*alfred.ModElement{
@@ -168,4 +173,48 @@ func main() {
 	}
 
 	items.End()
+}
+
+// newLine 针对 string 进行换行
+func newLine(translation string, newLineLimit int) string {
+	periods := strings.Split(translation, "。")
+	res := make([]string, 0)
+	for i := 0; i < len(periods); {
+		// 默认合并一句话
+		mergeNum, mergeString := merge(&periods, i, newLineLimit)
+		if mergeNum == 0 {
+			i++
+			continue
+		}
+
+		res = append(res, mergeString)
+		i = i + mergeNum
+	}
+	return strings.Join(periods, "\n")
+}
+
+const newLineLimit int = 50
+
+// merge 选取字符串数组里面 start开始的字符串 进行合并
+// 长度不超过newLineLimit限制
+// 如果有异常 返回 0 ,""
+// 没有异常返回 合并使用的字符串数 和 拼接好的字符串内容
+func merge(periods *[]string, start int, newLineLimit int) (int, string) {
+	// 计算需要合并 字符串数组中 几个字符串
+	wordCount := 0
+	if start < 0 || start >= len(*periods) {
+		return wordCount, ""
+	}
+
+	wordNumber := 0
+	for i := start; i < len(*periods); i++ {
+		if wordNumber > newLineLimit {
+			break
+		}
+		wordNumber += len((*periods)[i])
+		wordCount++
+	}
+
+	return wordCount, strings.Join((*periods)[start:start+wordCount], " ")
+
 }
